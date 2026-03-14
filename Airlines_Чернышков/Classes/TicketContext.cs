823d7681e11210430d356080ce7f8a1af5a27697
df275 <df275@EVIL>
2026-03-14 20:55:34 +0500

    Перед библиотекой. Чернышков

---
+++ Airlines_Чернышков/Classes/TicketContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Airlines_Чернышков.Models;
+
+namespace Airlines_Чернышков.Classes
+{
+    public class TicketContext : Ticket
+    {
+        public static List<TicketContext> AllTickets() {
+            List<TicketContext> allTickets = new List<TicketContext>();
+
+            return allTickets;
+        }
+    }
+}
